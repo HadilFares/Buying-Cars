87c6456ed652c1da97f45802c2409cc254177864
Mohamed <mohamed@pixelium.tn>
2020-12-29 19:27:22 +0100

    add + details+ list

--- src/app/add-car/add-car.component.ts
+++ src/app/add-car/add-car.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CarsService } from '../services/cars.service';
 import { Car } from '../shared/models/car';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-car',
@@ -8,21 +10,23 @@ import { Car } from '../shared/models/car';
   styleUrls: ['./add-car.component.css']
 })
 export class AddCarComponent implements OnInit {
-  
 
-  constructor(private carsService :CarsService) { }
+  public name;
+  public brand;
+  public description;
+  public price;
+  public year;
+  constructor(private carsService: CarsService) { }
 
   ngOnInit(): void {
+    this.name = "";
+    this.brand = "";
+    this.description = "";
+    this.price = null;
+    this.year = "";
   }
-  add(name,brand,price,description){
-    var x=new Car ();
-    x.name=name;
-    x.brand=brand;
-    x.price=price;
-    x.description=description;
-    this.carsService.add(x);
-    
-    
+  soumettre(f: NgForm) {
+    const { name, brand, description, price, year } = this;
+    this.carsService.add({ name, brand, description, price, year });
   }
-
 }
